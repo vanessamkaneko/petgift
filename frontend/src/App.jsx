314c9906ab8214746efcde7d1094a437ddf267fc
Vanessa Kaneko <vanessa.mitiek@gmail.com>
2026-03-25 19:19:05 -0300

    telas de cadastro e edição de pet prontas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Account from './pages/Account';
 import User from './pages/User';
+import PetRegister from './pages/PetRegister';
+import PetEdit from './pages/PetEdit';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/account" element={<Account />} />
         <Route path="/user/update" element={<User />} />
+        <Route path="/pet/register" element={<PetRegister />} />
+        <Route path="/pet/edit/:id" element={<PetEdit />} />
       </Routes>
     </Router>
   );
--- src/core/pet/dtos/FilterPets.dto.ts
+++ src/core/pet/dtos/FilterPets.dto.ts
@@ -1,10 +1,27 @@
-
+import { IsEnum, IsNumber, IsOptional } from "class-validator";
+import { Type } from "class-transformer";
 import { PetSex, PetSpecie, PetStatus } from "./CreatePet.dto";
 
-export type FilterPetsDTO = {
+export class FilterPetsDTO {
+  @IsEnum(PetStatus)
+  @IsOptional()
   status?: PetStatus;
+
+  @IsEnum(PetSpecie)
+  @IsOptional()
   species?: PetSpecie;
+
+  @IsEnum(PetSex)
+  @IsOptional()
   sex?: PetSex;
+
+  @Type(() => Number)
+  @IsNumber()
+  @IsOptional()
   minAge?: number;
+
+  @Type(() => Number)
+  @IsNumber()
+  @IsOptional()
   maxAge?: number;
-};
+}
